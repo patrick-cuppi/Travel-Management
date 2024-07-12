17cb7a9f3e8c8a0121cd3fa13f0f7b19dadb7d2c
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-07-12 18:27:48 -0300

    feat: send email

--- src/routes/create-trips.ts
+++ src/routes/create-trips.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import dayjs from "dayjs";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+import nodemailer from "nodemailer";
 
 export async function createTrip(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -10,18 +13,46 @@ export async function createTrip(app: FastifyInstance) {
                 destination: z.string().min(4),
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
+                owner_name: z.string(),
+                owner_email: z.string().email(),
             })
         },
     } , async (request) => {
-        const { destination, starts_at, ends_at } = request.body
+        const { destination, starts_at, ends_at, owner_name, owner_email } = request.body
+
+        if(dayjs(starts_at).isBefore(new Date())) {
+            throw new Error ('Invalid trip start date.')
+        }
+
+        if(dayjs(ends_at).isBefore(starts_at)) {
+            throw new Error ('Invalid trip end date.')
+        }
 
         const trip = await prisma.trip.create({
             data: {
                 destination,
                 starts_at,
-                ends_at
+                ends_at,
             }
         })
+
+        const mail = await getMailClient()
+
+        const message = await mail.sendMail({
+            from: {
+                name: 'Darth Vader',
+                address: 'vader@darkside.com',
+
+            },
+            to: {
+                name: owner_name,
+                address: owner_email,
+            },
+            subject: 'Testing...',
+            html: `<p>Testando o envio!</p>`
+        })
+
+        console.log(nodemailer.getTestMessageUrl(message))
 
         return { tripId: trip.id }
     })
