33a50d312f175bcf9aa66e320e31f67a62acc596
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-07-14 03:03:30 -0300

    feat: get links

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify()
 
@@ -25,6 +26,7 @@ app.register(confirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
+app.register(getLinks)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server Running!')
